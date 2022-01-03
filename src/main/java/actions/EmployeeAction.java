5b5fd462b776bca3d8084e74e21e9c3dd51aab82
sakurako.uesugi <cocb.1d2hznll.2525@gmail.com>
2022-01-03 19:59:41 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -58,4 +58,13 @@ public class EmployeeAction extends ActionBase {
      //一覧画面を表示
      forward(ForwardConst.FW_EMP_INDEX);
  }
+ /**
+  * 新規登録画面を表示する
+  */
+ public void entryNew() throws ServletException,IOException{
+     putRequestScope(AttributeConst.TOKEN,getTokenId());
+     putRequestScope(AttributeConst.EMPLOYEE,new EmployeeView()); //からの従業員インスタンス
+     //新規灯篭画面を表示
+     forward(ForwardConst.FW_EMP_NEW);
+ }
 }
